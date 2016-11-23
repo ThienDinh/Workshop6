e2f391da5c7593a629f2d103933d9f595db4bb65
Thien Dinh <tdinhcs@gmail.com>
2016-11-23 16:35:42 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var reverseString = require('./util').reverseString;
 var bodyParser = require('body-parser');
-var readDocument = require('./database').readDocument;
+var database = require('./database');
+var readDocument = database.readDocument;
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var validate = require('express-jsonschema').validate;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
 
 var app = express();
 
 // Use text parser functionality of bodyParser.
 app.use(bodyParser.text());
+app.use(bodyParser.json());
 // Server all files in the client/build directory.
 app.use(express.static('../client/build'));
+
+
 // GET feed
 app.get('/user/:userid/feed', function(req, res) {
 	var userid = req.params.userid;
@@ -20,6 +28,50 @@ app.get('/user/:userid/feed', function(req, res) {
 		res.status(401).end();
 	}
 });
+// POST /feeditem { userId: user, location: location, contents: contents}
+app.post('/feeditem', validate({body: StatusUpdateSchema}), function(req, res) {
+	var body = req.body;
+	var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+	if (fromUser === body.userId) {
+		var newUpdate = postStatusUpdate(body.userId, body.location,
+			body.contents);
+		res.status(201);
+		res.set('Location', '/feeditem' + newUpdate._id);
+
+		res.send(newUpdate);
+	} else {
+		res.status(401).end();
+	}
+});
+
+
+// Post FeedItem.
+function postStatusUpdate(user, location, contents) {
+	var time = new Date().getTime();
+	var newStatusUpdate = {
+		'likeCounter': [],
+		'type': 'statusUpdate',
+		'contents': {
+			'author': user,
+			'postDate': time,
+			'location': location,
+			'contents': contents,
+			'likeCounter': []
+		},
+		'comments': []
+	};
+
+	newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+
+	var userData = readDocument('users', user);
+	var feedData = readDocument('feeds', userData.feed);
+	feedData.contents.unshift(newStatusUpdate._id);
+
+	writeDocument('feeds', feedData);
+
+	return newStatusUpdate;
+}
 
 // Retrieve Id from token.
 function getUserIdFromToken(authorizationLine) {
@@ -69,6 +121,15 @@ function getFeedData(user, cb) {
   // Return FeedData with resolved references.
   return feedData;
 }
+
+
+app.use(function(err, req, res, next) {
+	if (err.name === 'JsonSchemaValidation') {
+		res.status(400).end();
+	} else {
+		next(err);
+	}
+});
 
 app.listen(3000, function() {
 	console.log('Example app listening on port 3000!');
